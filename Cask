5511a27723d3abf658f2cdeb78558df5b021871f
oliver <oflatt@gmail.com>
2018-12-08 11:36:58 -0700

    fix path again

---
+++ Cask
@@ -0,0 +1,3 @@
+(source melpa)
+(package-file "esonify.el")
+(depends-on "sound-wav")
--- esonify.el
+++ esonify.el
@@ -1,15 +1,18 @@
 
+(setq debug-on-error t)
 (defvar esoundifyonp t "if esoundify is on")
 
-(defconst soundpath (substitute-in-file-name "$HOME/esonify/sounds"))
-
+(defconst soundpath (expand-file-name "~/esonify/sounds"))
+(print soundpath)
 (defun makesound ()
   (if esoundifyonp
-      (if (integerp last-command-event)
-	  (cond ((and (>= last-command-event 97) (<= last-command-event 127))
-		 (sound-wav-play (concat soundpath "/sine" (number-to-string (- last-command-event 96)) "_1" ".wav")))
-		((and (>= last-command-event 65) (<= last-command-event 90))
-		 (sound-wav-play (concat soundpath "/square" (number-to-string (- last-command-event 64)) "_25" ".wav")))))))
+      (progn
+	(if (integerp last-command-event)
+	    (cond ((and (>= last-command-event 97) (<= last-command-event 127))
+		   (sound-wav-play (concat soundpath "/sine" (number-to-string (- last-command-event 96)) "_1" ".wav")))
+		  ((and (>= last-command-event 65) (<= last-command-event 90))
+		   (sound-wav-play (concat soundpath "/square" (number-to-string (- last-command-event 64)) "_25" ".wav"))))))))
+
 
 (add-hook 'post-command-hook 'makesound)
 
