d3d5b439bb6656d6b9bd2f11e49994eef9bfbfbc
oliver <oflatt@gmail.com>
2018-12-13 10:07:46 -0700

    path again

--- esonify.el
+++ esonify.el
@@ -30,7 +30,10 @@
 
 (defvar esoundifyonp t "if esoundify is on")
 
-(defconst soundpath (expand-file-name "~/esonify/sounds"))
+(defconst esonify--el-source-dir
+  (file-name-directory (or load-file-name (buffer-file-name))))
+  
+(defconst soundpath (expand-file-name "./sounds/" esonify--el-source-dir))
 
 (defconst alphabetmap #s(hash-table size 26 data (122 0 120 1 99 2 118 3 98 4 110 5 109 6 97 7 115 8 100 9 102 10 103 11 104 12 106 13 107 14 108 15 113 16 119 17 101 18 114 19 116 20 121 21 117 22 105 23 111 24 112 25 )))
 
