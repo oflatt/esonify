bb5c121cbf1a173514149477875791224297808a
oliver <oflatt@gmail.com>
2018-12-13 10:02:02 -0700

    prep for melpa

--- esonify.el
+++ esonify.el
@@ -1,4 +1,32 @@
+;;; esonify.el --- Sonify your code
 
+;; Copyright (C) 2018 by Oliver Flatt
+
+;; Author: Oliver Flatt <oflatt@gmail.com>
+;; URL: https://github.com/oflatt/esonify
+;; Package-Version: 1
+;; Version: 0.02
+;; Package-Requires: ((sound-wav "0.02"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; esonify makes characters typed audible through different types of sound waves, also reading code to you through sound.
+;; toggle-esonify turns it on and off
+
+;;; Code:
 
 (defvar esoundifyonp t "if esoundify is on")
 
