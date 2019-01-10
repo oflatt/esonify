bdc79d4ab2e3c449b5bef46e5cabc552beeed5c6
oliver <oflatt@gmail.com>
2019-01-10 09:21:22 -0700

    update description

--- esonify.el
+++ esonify.el
@@ -22,8 +22,9 @@
 
 ;;; Commentary:
 
-;; esonify makes characters typed audible through different types of sound waves, also reading code to you through sound.
-;; toggle-esonify turns it on and off
+;; M-x esonify-mode
+;; An emacs extension that sonifies your code. Skilled users will begin to be able to recognize some code based solely upon the music it generates.
+;; Hear sine waves at different frequencies for lowercase letters, square waves for upper case letters, and triangle waves for keybindings.
 
 ;;; Code:
 
